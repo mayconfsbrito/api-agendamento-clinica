62fb6fbb9e7e0bfa0dd357c52c65bdfc8dcf81c7
Maycon Brito <mayconfsbrito@gmail.com>
2020-02-07 00:19:56 -0300

    Installed typescript and express

---
+++ js/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const app = express();
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
